7c2e683f2445c417c246ec0629bdb752be351661
Hellek-Greed <nimnarr@gmail.com>
2022-12-15 16:22:50 +0300

    while added

--- homework_7/task/main.py
+++ homework_7/task/main.py
@@ -1,14 +1,16 @@
 import request, db
 
 def main():
-    ansver = request.action()
-    if ansver == 1:
-        data = db.data_collector()
-        db.db_save(data)
+    ansver = 1
+    while ansver:
+        ansver = request.action()
+        if ansver == 1:
+            data = db.data_collector()
+            db.db_save(data)
 
-    if ansver == 2:
-        db_view = request.db_view()
-        db.db_print(db_view)
+        if ansver == 2:
+            db_view = request.db_view()
+            db.db_print(db_view)
 
 if __name__ == '__main__':
     main()
