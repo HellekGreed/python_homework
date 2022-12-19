8b5917098536456ec5696a2e87c6ddfab82e8369
Hellek-Greed <nimnarr@gmail.com>
2022-12-19 10:51:32 +0300

    requests added

---
+++ homework_8/task/requests.py
@@ -0,0 +1,19 @@
+
+def change_menu():
+    change = int(input('1 - Войти в ситему\n0 - Выйти из программы\n'))
+    return change
+
+def change_accaunt():
+    change = int(input('Выберете тип аккаунта\n1 - Учитель\n2 - Ученик\n'))
+    return change
+
+def teacher_function():
+    change = int(input('Выберете операцию\n1 - Поставить оценку ученику\n2 - Узнать оценки ученика\n0 - Выйти\n'))
+    return change
+
+def change_subject():
+    change = int(input('1 - Вывести все оценки\n2 - Ввести название предмета\n'))
+    return change
+def change_rework():
+    change = int(input('1 - Продолжить работу\n0 - Выйти из программы\n'))
+    return change
