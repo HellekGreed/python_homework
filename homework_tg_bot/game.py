2bfb793e3e6324f876a02382d57afe27d0fc691f
Hellek-Greed <nimnarr@gmail.com>
2022-12-23 17:47:25 +0300

    game added

--- homework_tg_bot/game.py
+++ homework_tg_bot/game.py
@@ -1,31 +1,73 @@
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from random import randint
 
-def f(x):
-    if  x < 29:
-        return 28
-    elif 28 < x < 57:
-        return x - 29
-    else:
-        return randint(1, 29)
+TOKEN = '5742785255:AAGfKKBs3mKhaNiY2Q01Zt_L9cKROccWmVQ'
+bot = Bot(token=TOKEN)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
 
-def game(update, context):
-    count_of_sticks = 120
-    gamer_1 = 'Вы'
-    gamer_2 = 'Bot'
-    current_gamer = gamer_1
-    while count_of_sticks > 0:
-        context.bot.send_message(update.effective_chat.id, f'количество оставшихся палочек: {count_of_sticks}')
-        while True:
-            if current_gamer == gamer_1:
-                context.bot.send_message(update.effective_chat.id, 'Ваш ход (1 - 28): ')
-                number_to_delete = int(update.message.text)
-                if 1 <=  number_to_delete <= 28:
-                    break
-            elif current_gamer == gamer_2:
-                number_to_delete = f(count_of_sticks)
-                context.bot.send_message(update.effective_chat.id, 'Ваш ход: (1 - 28): ', number_to_delete)
-                break
-        count_of_sticks -= number_to_delete
-        current_gamer = gamer_2 if current_gamer == gamer_1 else gamer_1
-    current_gamer = gamer_1 if current_gamer == gamer_2 else gamer_2
-    context.bot.send_message(update.effective_chat.id, f'Победил {current_gamer}')
+A = 0
+B = 1
+C = 2
+D = 3
+count_of_sticks = 120
+
+def start(update, context):
+    context.bot.send_message(update.effective_chat.id, 'Здравствуйте!\nВведите число от 1 до 28 что бы начать игру ')
+    return A
+
+def player_turn(update, context):
+    global count_of_sticks
+    context.bot.send_message(update.effective_chat.id, f'Ваш ход (1 - 28): {update.message.text}')
+    count_of_sticks -= int(update.message.text)
+    context.bot.send_message(update.effective_chat.id, f'Осталось: {count_of_sticks}')
+
+
+    if count_of_sticks > 0:
+        context.bot.send_message(update.effective_chat.id, 'Введите что угодно что бы продолжить')
+        return B
+    else:
+        context.bot.send_message(update.effective_chat.id, 'Ты победил!')
+        return D
+def bot_turn(update, context):
+    global count_of_sticks
+    if count_of_sticks < 29:
+        number_to_delete = count_of_sticks
+    elif 28 < count_of_sticks < 57:
+        number_to_delete = count_of_sticks - 29
+    else:
+        number_to_delete = randint(1, 29)
+    context.bot.send_message(update.effective_chat.id, f'Мой ход: {number_to_delete}')
+    count_of_sticks -= number_to_delete
+    context.bot.send_message(update.effective_chat.id, f'Осталось: {count_of_sticks}')
+
+    if count_of_sticks > 0:
+        context.bot.send_message(update.effective_chat.id, 'Введите число от 1 до 28')
+        return C
+    else:
+        context.bot.send_message(update.effective_chat.id, 'Я победил!')
+        return D
+
+def end(update, context):
+    context.bot.send_message(update.effective_chat.id, 'END')
+    return ConversationHandler.END
+
+
+
+
+start_handler = CommandHandler('start', start)
+player_turn_handler = MessageHandler(Filters.text, player_turn)
+bot_turn_handler = MessageHandler(Filters.text, bot_turn)
+end_handler = CommandHandler('end', start)
+
+
+conv_handler = ConversationHandler(entry_points=[start_handler],
+                                   states={A: [player_turn_handler],
+                                           B: [bot_turn_handler],
+                                           C: [player_turn_handler]},
+                                   fallbacks=[end_handler])
+dispatcher.add_handler(conv_handler)
+
+updater.start_polling()
+updater.idle()
